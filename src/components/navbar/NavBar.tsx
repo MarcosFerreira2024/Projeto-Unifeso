1c79ca7e86d9373e60bb1b03547411f782f9bbe3
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-14 06:34:34 -0300

    Update NavBar.tsx

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,51 +1,84 @@
-"use client"
-import React from 'react'
-import Image from 'next/image'
-import BotoesNavBar from './BotoesNavBar'
+"use client";
+import React from "react";
+import Image from "next/image";
+import BotoesNavBar from "./BotoesNavBar";
 
 function NavBar() {
   function handleVisibility() {
-    const menuMobile = document.getElementById('menu-mobile') as HTMLElement
-    if (menuMobile.classList.contains('hidden')){
-        menuMobile.classList.remove('hidden',)
-        menuMobile.classList.add('flex','top-[120px]','right-0')
-        setTimeout(() => {
-            menuMobile.classList.add('bg-background','rounded-bl-sm')
-            menuMobile.classList.remove('translate-x-[120px]','opacity-0')
-        }, 150);
-    }else{
-        menuMobile.classList.add('translate-x-[120px]','opacity-0')
-        setTimeout(()=>{
-            menuMobile.classList.add('hidden')
-        },150)
+    const menuMobile = document.getElementById("menu-mobile") as HTMLElement;
+    if (menuMobile.classList.contains("hidden")) {
+      menuMobile.classList.remove("hidden");
+      menuMobile.classList.add("flex", "top-[120px]", "right-0");
+      setTimeout(() => {
+        menuMobile.classList.add("bg-background", "rounded-bl-sm");
+        menuMobile.classList.remove("translate-y-[-40px]", "opacity-0");
+      }, 300);
+    } else {
+      menuMobile.classList.add("translate-y-[-40px]", "opacity-0");
+      setTimeout(() => {
+        menuMobile.classList.add("hidden");
+      }, 300);
     }
-   
   }
 
   return (
-    <div className='bg-background fixed w-[100vw] min-h-[120px]'>
-      <div className='max-w-[1440px] min-h-[120px] flex mx-auto justify-between px-5 md:px-0 items-center'>
-        <Image src="NavBar/unifeso.svg" alt='logo unifeso' width={250} height={66} />
-        
+    <div className="bg-background fixed w-[100vw] min-h-[120px]">
+      <div className="max-w-[1440px] min-h-[120px] flex mx-auto justify-between px-5 md:px-0 items-center">
+        <Image
+          src="NavBar/unifeso.svg"
+          alt="logo unifeso"
+          width={250}
+          height={66}
+        />
+
         <nav
-          className='hidden md:flex-row flex-col opacity-0 md:opacity-100 md:flex p-10 rounded-bl-lg md:p-0 gap-5 transition-all ease-in translate-x-[120px] md:translate-x-0 md:static absolute'
-          id='menu-mobile'
+          className="hidden md:flex-row flex-col opacity-0 md:opacity-100 md:flex p-10 w-[100vw] justify-center md:justify-end items-center  rounded-bl-lg md:p-0 gap-5 transition-all ease-in translate-y-[-40px] md:translate-y-0 md:static absolute"
+          id="menu-mobile"
         >
-          <BotoesNavBar href='#agendamento' src='NavBar/agenda.svg' alt='agendamento'/>
-          <BotoesNavBar href='#servicos' src='NavBar/services.svg' alt='servicos'/>
-          <BotoesNavBar href='#notificacoes' src='NavBar/notifications.svg' alt='notificações'/>
-          <BotoesNavBar href='#feedback' src='NavBar/feedback.svg' alt='feedback'/>
-          <BotoesNavBar href='#contato' src='NavBar/contact.svg' alt='contato'/>
-          <BotoesNavBar href='/cadastro' src='NavBar/account.svg' alt='cadastrar'/>
+          <BotoesNavBar
+            href="#agendamento"
+            src="NavBar/agenda.svg"
+            alt="agendamento"
+          />
+          <BotoesNavBar
+            href="#servicos"
+            src="NavBar/services.svg"
+            alt="servicos"
+          />
+          <BotoesNavBar
+            href="#notificacoes"
+            src="NavBar/notifications.svg"
+            alt="notificações"
+          />
+          <BotoesNavBar
+            href="#feedback"
+            src="NavBar/feedback.svg"
+            alt="feedback"
+          />
+          <BotoesNavBar
+            href="#contato"
+            src="NavBar/contact.svg"
+            alt="contato"
+          />
+          <BotoesNavBar
+            href="/cadastro"
+            src="NavBar/account.svg"
+            alt="cadastrar"
+          />
         </nav>
-        <nav className='md:hidden'>
+        <nav className="md:hidden">
           <button onClick={handleVisibility}>
-            <Image src="NavBar/menu-hamburguer.svg" alt="menu mobile" width={24} height={10} />
+            <Image
+              src="NavBar/menu-hamburguer.svg"
+              alt="menu mobile"
+              width={24}
+              height={10}
+            />
           </button>
         </nav>
       </div>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
