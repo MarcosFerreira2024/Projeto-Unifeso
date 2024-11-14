6466d3648a285805498ef00880aee6bbd324c4f5
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-14 03:53:32 -0300

    melhorando nav-bar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
 import BotoesNavBar from './BotoesNavBar'
@@ -6,17 +7,17 @@ function NavBar() {
   function handleVisibility() {
     const menuMobile = document.getElementById('menu-mobile') as HTMLElement
     if (menuMobile.classList.contains('hidden')){
-        menuMobile.classList.remove('hidden')
-        menuMobile.classList.add('flex','opacity-0','absolute','md:static','right-0','p-5','translate-x-[120px]','md:translate-x-[0px]')
+        menuMobile.classList.remove('hidden',)
+        menuMobile.classList.add('flex','top-[120px]','right-0')
         setTimeout(() => {
-            menuMobile.classList.add('opacity-100','translate-x-[0px]', 'bg-background','rounded-bl-sm','top-[120px]',)
-
-        }, 100);
+            menuMobile.classList.add('bg-background','rounded-bl-sm')
+            menuMobile.classList.remove('translate-x-[120px]','opacity-0')
+        }, 150);
     }else{
-        menuMobile.classList.remove('opacity-100','translate-x-[0px]')
+        menuMobile.classList.add('translate-x-[120px]','opacity-0')
         setTimeout(()=>{
-            menuMobile.classList.add('hidden','opacity-100')
-        },100)
+            menuMobile.classList.add('hidden')
+        },150)
     }
    
   }
@@ -27,7 +28,7 @@ function NavBar() {
         <Image src="NavBar/unifeso.svg" alt='logo unifeso' width={250} height={66} />
         
         <nav
-          className='hidden md:flex-row flex-col md:flex gap-5  transition-all ease-in '
+          className='hidden md:flex-row flex-col opacity-0 md:opacity-100 md:flex p-5 md:p-0 gap-5 transition-all ease-in translate-x-[120px] md:translate-x-0 md:static absolute'
           id='menu-mobile'
         >
           <BotoesNavBar href='#agendamento' src='NavBar/agenda.svg' alt='agendamento'/>
