01699dd7e2c1d0d8f85f46b3ab2e80a72a74ae1e
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-16 04:46:29 -0300

    Create marquee-data.ts

---
+++ src/data/marquee-data.ts
@@ -0,0 +1,56 @@
+
+type cards = {
+    tipo: string,
+    horario_inicio:number,
+    horario_termino:number,
+
+}
+
+export let cards:cards[]= [
+    {
+        tipo: "Consulta ao CPF",
+        horario_inicio: 8,
+        horario_termino: 16,
+    },
+    {
+        tipo: "Emissão de DAS",
+        horario_inicio: 8,
+        horario_termino: 17,
+    },
+    {
+        tipo: "Cadastro CPF",
+        horario_inicio: 9,
+        horario_termino: 19,
+    },
+    {
+        tipo: "Declaração de IRPF",
+        horario_inicio: 9,
+        horario_termino: 17,
+    },
+    {
+        tipo: "MEI",
+        horario_inicio: 9,
+        horario_termino: 18,
+    },
+    {
+        tipo: "Planejamento Financeiro",
+        horario_inicio: 9,
+        horario_termino: 15,
+    },
+    {
+        tipo: "Auxílio eSocial",
+        horario_inicio: 10,
+        horario_termino: 16,
+    },
+    {
+        tipo: "Emissão de Documentos",
+        horario_inicio: 10,
+        horario_termino: 18,
+    },
+    {
+        tipo: "Educação Fiscal",
+        horario_inicio: 14,
+        horario_termino: 17,
+    },
+    
+];
