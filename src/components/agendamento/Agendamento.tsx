c386b0a82592e1ca95e415c4b9265e01ffbea0a5
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-16 19:54:37 -0300

    começo  da logica do slider Agendamento

--- src/components/agendamento/Agendamento.tsx
+++ src/components/agendamento/Agendamento.tsx
@@ -4,15 +4,30 @@ import Titulo from "../geral/Titulo";
 
 function Agendamento() {
   return (
-    <section className="mt-20  flex-col mx-auto max-w-[1280px] gap-[60px]  flex justify-center">
-      <div className="mx-auto max-w-[1280px]">
+    <section id="agendamento">
+      <div className="mx-auto max-w-[1280px] flex justify-center mt-20 mb-20">
         <Titulo titulo="Agendamento" />
       </div>
-      <div className="flex flex-wrap gap-[180px] justify-center max-w-[1280px] ">
-        <CardAgendamento label="Cancelamento" src="/Agendamento/Cancelamento.svg" color="#DD2929" />
-        <CardAgendamento label="Agendar" src="/Agendamento/Agendar.svg" color="#04887F"/>
-        <CardAgendamento label="Reagendamento" src="/Agendamento/Reagendar.svg" color="#2563EB"/>
+      <div className="slider">
+        <div className="slides">
+            <div className="slide">
+                <CardAgendamento label="Cancelamento" src="/Agendamento/Cancelamento.svg" color="#DD2929" />
+                <CardAgendamento label="Agendar" src="/Agendamento/Agendar.svg" color="#04887F"/>
+                <CardAgendamento label="Reagendamento" src="/Agendamento/Reagendar.svg" color="#2563EB"/>
+            </div>
+            <div className="slide">
+                <CardAgendamento label="Cancelamento" src="/Agendamento/Cancelamento.svg" color="#DD2929" />
+                <CardAgendamento label="Teste2" src="/Agendamento/Agendar.svg" color="#04887F"/>
+                <CardAgendamento label="Reagendamento" src="/Agendamento/Reagendar.svg" color="#2563EB"/>
+            </div>
+            <div className="slide">
+                <CardAgendamento label="Cancelamento" src="/Agendamento/Cancelamento.svg" color="#DD2929" />
+                <CardAgendamento label="Teste3" src="/Agendamento/Agendar.svg" color="#04887F"/>
+                <CardAgendamento label="Reagendamento" src="/Agendamento/Reagendar.svg" color="#2563EB"/>
+            </div>
+        </div>
       </div>
+
     </section>
   );
 }
--- src/components/agendamento/CardAgendamento.tsx
+++ src/components/agendamento/CardAgendamento.tsx
@@ -1,17 +1,45 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-function CardAgendamento({label,src,height=40,width=40,color="red"}:{label:string,height?:number,width?:number,src:string,color?:string}) {
+function CardAgendamento({
+  label,
+  src,
+  height = 40,
+  width = 40,
+  color = "red",
+}: {
+  label: string;
+  height?: number;
+  width?: number;
+  src: string;
+  color?: string;
+}) {
+  const [hover,setHover] = React.useState(false)
+
+  function handleMouseEnter(){
+    setHover(true)
+  }
+  function handleMouseLeave(){
+    setHover(false)
+  }
   return (
-    <div className='flex-1 min-w-[284px] max-w-[284px] min-h-[400px] max-h-[400px] rounded-[10px] flex  flex-col justify-center gap-[60px] items-center' style={{border: `2px solid ${color}`, color: color}}>
-        <div className='w-[90px] h-[90px] rounded-full flex items-center justify-center'style={{border:`2px solid ${color}`}}>
-            <Image src={src} height={height} width={width} alt={label}/>
+    
+      <div
+        className={`min-w-[284px] max-w-[284px] min-h-[400px] max-h-[400px]  cursor-pointer rounded-[10px]  flex  flex-col justify-center gap-[60px] items-center`}
+        style={{ border: `2px solid ${color}`, color: hover? "white": color, backgroundColor: hover? color : "white", transition:"all 0.5s linear"}} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
+      >
+        <div
+          className="w-[90px] h-[90px] z-10 bg-white rounded-full shadowMarquee flex items-center justify-center"
+          style={{ border: `2px solid ${color}` }}
+        >
+          <Image src={src} height={height} width={width} alt={label} />
         </div>
         <div>
-            <h1 className='text-2xl font-roboto'>{label}</h1>
+          <h1 className="text-3xl font-roboto">{label}</h1>
         </div>
-    </div>
-  )
+      </div>
+    
+  );
 }
 
-export default CardAgendamento
+export default CardAgendamento;
