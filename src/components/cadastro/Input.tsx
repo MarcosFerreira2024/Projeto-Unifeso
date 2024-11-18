9e6e960f125c2e1e0e8d7548e8ede523e9c14079
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-18 17:40:51 -0300

    Update Input.tsx

--- src/components/cadastro/Input.tsx
+++ src/components/cadastro/Input.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 type input = {
-  type:string,
+  type : "text" | "password" | "email" | "number" | "tel" | "url" | "search" | "date" | "time" | "datetime-local" | "month" | "week" | "file" | "checkbox" | "radio" | "range" | "color" | "submit" | "reset" | "button";
   id:string,
   label:string,
   outros?: React.InputHTMLAttributes<HTMLInputElement>;
+  length?:number
 }
-function Input({type,id,label,outros}:input) {
+function Input({type,id,label,outros,length}:input) {
   return (
     <div className="flex flex-col">
       <label htmlFor={id} className="text-title font-roboto text-xl">
@@ -13,9 +14,10 @@ function Input({type,id,label,outros}:input) {
       </label>
       <input
         type={type}
+        maxLength={length}
         {...outros}
         id={id}
-        className="w-[266px]  placeholder:focus:text-white hover:bg-background placeholder:text-title px-5 botao outline-none h-[38px] border-[2px] border-subtitle focus:bg-[#2e8b85] bg-white  focus:text-[white] transition-all ease-linear text-title  font-roboto rounded-[10px] "
+        className="w-[233px]  placeholder:focus:text-white hover:bg-background placeholder:text-title px-5 botao outline-none h-[38px] border-[2px] border-subtitle focus:bg-[#2e8b85] bg-white  focus:text-[white] transition-all ease-linear text-title  font-roboto rounded-[10px] "
       />
     </div>
   );
