4c899f95e22390ddd53dd4a498468347844ca643
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-18 17:40:34 -0300

    Update Cadastro.tsx

--- src/components/cadastro/Cadastro.tsx
+++ src/components/cadastro/Cadastro.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Titulo from "../geral/Titulo";
 import Texto from "../geral/Texto";
-import Input from "./Input";
-import BotaoForm from "./BotaoForm";
 import Image from "next/image";
 import Link from "next/link";
+import FormEntrar from "./FormEntrar";
+import Registro from "./Registro";
 function Cadastro() {
+  const [entrar,setEntrar]=React.useState(true)
   return (
     <section className="grid md:grid-cols-2">
       <section className=" bg-background md:h-screen h-[60vh]">
-        <div className="max-w-[640px] ml-auto flex    min-w-[100%] pt-60 justify-center md:justify-normal items-center flex-col md:h-screen h-[60vh]">
+        <div className="max-w-[640px] ml-auto flex    min-w-[100%] pt-40 md:pt-60 justify-center md:justify-normal items-center flex-col md:h-screen h-[60vh]">
 
           <div className="flex flex-col gap-5 relative ">
             <div className="absolute top-[-217px] left-[25%] md:left-0">
@@ -38,42 +39,9 @@ function Cadastro() {
           </div>
         </div>
       </section>
-      <section className=" bg-white md:h-screen h-[60vh]   ">
+      <section className=" bg-white md:h-screen h-screen sombraRegistro  ">
         <div className="max-w-[640px] flex flex-col items-center gap-5 pt-60 justify-center md:justify-normal  min-w-[100%] md:h-screen h-[60vh]">
-          <div className="flex flex-col gap-5">
-            <div>
-              <Titulo titulo="Entrar" />
-            </div>
-            <div>
-              <form action="post" className="flex flex-col gap-5">
-                <Input
-                  type="email"
-                  label="Email"
-                  id="email"
-                  outros={{ required: true }}
-                />
-                <Input
-                  type="password"
-                  label="Senha"
-                  id="Senha"
-                  outros={{ required: true }}
-                />
-                <BotaoForm label="Enviar" action="submit" />
-              </form>
-              <div className="text-title mt-2 text-lg">
-                <h1>
-                  Nao possui conta ?{" "}
-                  <button
-                    className="text-subtitle underline"
-                    onClick={() => "dps faço isso aqui"}
-                  >
-                    {" "}
-                    Registre-se
-                  </button>
-                </h1>
-              </div>
-            </div>
-          </div>
+          {entrar?<FormEntrar onSwitch={()=> setEntrar(!entrar)}/>:<Registro onSwitch={()=> setEntrar(!entrar)} />}
         </div>
       </section>
     </section>
