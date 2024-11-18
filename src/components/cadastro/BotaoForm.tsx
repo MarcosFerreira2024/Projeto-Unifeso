0f746635d847c813c759bc654d6ac84d942b0bf1
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-18 17:40:29 -0300

    Update BotaoForm.tsx

--- src/components/cadastro/BotaoForm.tsx
+++ src/components/cadastro/BotaoForm.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 type formbotao ={
     action?: "button" | "submit" | "reset";
     label: string; 
+    outros?: React.ButtonHTMLAttributes<HTMLButtonElement>; 
 
 }
-function BotaoForm({action="submit",label}: formbotao){
+function BotaoForm({action="submit",label,outros}: formbotao){
   return (
-            <button className='w-[133px] self-center   bg-background  px-5 botao outline-none h-[38px] border-[2px] hover:bg-white hover:text-title border-subtitle focus:bg-[white]   focus:text-title transition-all ease-linear text-white  font-roboto rounded-[10px] ' type={action}>{label}</button>
+            <button {...outros} className='w-[133px] self-center   bg-background  px-5 botao outline-none h-[38px] border-[2px] hover:bg-white hover:text-title border-subtitle focus:bg-[white]   focus:text-title transition-all ease-linear text-white  font-roboto rounded-[10px] ' type={action}>{label}</button>
   )
 }
 
