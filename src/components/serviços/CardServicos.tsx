a236b90c656a94c74c67a9d09595950e12aecacf
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-19 17:09:42 -0300

    melhorando a logica dos cards

--- src/components/serviços/CardServicos.tsx
+++ src/components/serviços/CardServicos.tsx
@@ -1,17 +1,22 @@
 import Image from 'next/image'
 import React from 'react'
+import ModalServico from './ModalServico';
+import { ContextService } from './ContextServico';
 type card = {
     url?:string; 
     alt:string; 
     width?:number; 
     height?:number; 
-    titulo:string; 
+    titulo:string;
+    data:string; 
 }
-function CardServicos({titulo,url="/servicos/agenda.svg",alt,height=81,width=81}:card) {
+function CardServicos({titulo,url="/servicos/agenda.svg",alt,height=81,width=81,data}:card) {
+
+    const {openModal} = React.useContext(ContextService)
 
   return (
-    <div className=' min-h-[250px] sm:min-h-[350px] min-w-[250px] sm:min-w-[350px] max-h-[250px] sm:max-h-[350px] max-w-[250px] sm:max-w-[350px]  mx-auto flex-1 hover:border-[4px]  hover:border-[#f3f3f3] border-[4px] border-[transparent] flex-wrap rounded-[20px] flex justify-around flex-col cardservicos bg-white'>
-        <div className='flex hover:opacity-80 flex-col text-center gap-2'>
+    <div role='button' onClick={(event) => openModal(event)} data-descricao={data as string}  className='min-h-[250px] hover:opacity-80  sm:min-h-[350px] min-w-[250px] sm:min-w-[350px] max-h-[250px] sm:max-h-[350px] max-w-[250px] sm:max-w-[350px]  mx-auto flex-1 hover:border-[4px]  hover:border-[#f3f3f3] border-[4px] border-[transparent] flex-wrap rounded-[20px] flex justify-around flex-col cardservicos bg-white'>
+        <div className='flex flex-col text-center gap-2'>
             <div className='flex self-center' >
                 <Image src={url} alt={alt} height={height} width={width}   className='object-fit object-bottom min-h-[81px] '/>
             </div>
@@ -19,6 +24,7 @@ function CardServicos({titulo,url="/servicos/agenda.svg",alt,height=81,width=81}
                 <h1 className='sm:text-3xl text-xl  text-title font-roboto'>{titulo}</h1>
             </div>
         </div>
+        
     </div>
   )
 }
