0d80382567e5b765c5498b9e1c14a7e5a44ebf43
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-20 20:25:05 -0300

    dark mode input

--- src/components/cadastro/Input.tsx
+++ src/components/cadastro/Input.tsx
@@ -9,7 +9,7 @@ type input = {
 function Input({type,id,label,outros,length}:input) {
   return (
     <div className="flex flex-col">
-      <label htmlFor={id} className="text-title font-roboto text-xl">
+      <label htmlFor={id} className="text-title dark:text-gray-300 font-roboto text-xl">
         {label} :
       </label>
       <input
@@ -17,7 +17,7 @@ function Input({type,id,label,outros,length}:input) {
         maxLength={length}
         {...outros}
         id={id}
-        className="w-[250px]  placeholder:focus:text-white hover:bg-background placeholder:text-title px-5 botao outline-none h-[38px] border-[2px] border-subtitle focus:bg-[#2e8b85] bg-white  focus:text-[white] transition-all ease-linear text-title  font-roboto rounded-[10px] "
+        className="w-[250px]   placeholder:focus:text-white hover:bg-background dark:hover:bg-gray-300 dark:focus:bg-gray-300 dark:placeholder:focus:text-gray-600 placeholder:text-title px-5 botao outline-none h-[38px] border-[2px] border-subtitle dark:border-gray-600 focus:bg-[#2e8b85] bg-white dark:bg-gray-600  focus:text-[white] dark:focus:text-gray-600 dark:text-gray-300 dark:hover:text-gray-500 dark:placeholder:text-gray-300  transition-all ease-linear text-title  font-roboto rounded-[10px] "
       />
     </div>
   );
