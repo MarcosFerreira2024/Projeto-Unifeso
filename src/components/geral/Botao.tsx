5d8bd4f72ed8b4c5f2c1eee40b751d6f13099f34
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-20 20:56:58 -0300

    atualizando css / mudando animação do scroll

--- src/components/geral/Botao.tsx
+++ src/components/geral/Botao.tsx
@@ -17,7 +17,7 @@ function Botao({
   return (
     <div className="pt-5">
       <button
-        className="w-[168px] botao h-[38px] border-[2px] border-subtitle hover:bg-[#2e8b85] dark:border-gray-600 bg-white dark:bg-pagina-dark dark:text-gray-500 dark:hover:bg-[white] dark:hover:text-background-dark  focus:bg-[#2e8b85] dark:focus:bg-[white] dark:focus:text-background-dark focus:text-title  hover:text-[white] transition-all ease-linear text-title  font-roboto text-base rounded-[10px] "
+        className="w-[168px] botao h-[38px] border-[2px] border-subtitle hover:bg-[#2e8b85] dark:border-gray-300 bg-white dark:bg-pagina-dark dark:text-gray-300 dark:hover:bg-[white] dark:hover:text-background-dark  focus:bg-[#2e8b85] dark:focus:bg-[white] dark:focus:text-background-dark focus:text-title  hover:text-[white] transition-all ease-linear text-title  font-roboto text-base rounded-[10px] "
         type={action}
         onClick={handleClick}
       >
