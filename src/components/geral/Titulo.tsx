f5dd3659ae075f4ac97d6f20fd1106ba1e93a478
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-20 20:25:31 -0300

    dark mode titulo

--- src/components/geral/Titulo.tsx
+++ src/components/geral/Titulo.tsx
@@ -6,9 +6,9 @@ type cabecalho = {
 }
 function Titulo({subtitulo, titulo,color}:cabecalho) {
   return (
-    <div>
-      <h1 className='lg:text-6xl text-5xl sm:text-5xl font-roboto text-title  text-nowrap'style={{color:color}}>{titulo}</h1>
-      <p className='lg:text-xl text-lg font-roboto text-subtitle ' style={{color:color}}>{subtitulo}</p>
+    <div >
+      <p className='lg:text-xl text-lg font-roboto text-subtitle dark:text-subtitle-dark ' style={{color:color}}>{subtitulo}</p>
+      <h1 className='lg:text-6xl text-5xl sm:text-5xl font-roboto text-title dark:text-title-dark  text-nowrap'style={{color:color}}>{titulo}</h1>
     </div>
   )
 }
