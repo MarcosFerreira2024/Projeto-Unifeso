1c68385af36f4e7c5375197ab249cb0622f87023
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-20 20:24:40 -0300

    dark mode Card Agendamento

--- src/components/agendamento/CardAgendamento.tsx
+++ src/components/agendamento/CardAgendamento.tsx
@@ -25,14 +25,14 @@ function CardAgendamento({
   return (
     
       <div
-        className={` h-[400px] w-[284px]   cursor-pointer rounded-[10px]  flex  flex-col justify-center gap-[60px] items-center`}
-        style={{ border: `2px solid ${color}`, color: hover? "white": color, backgroundColor: hover? color : "white", transition:"all 0.5s linear"}} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
+        className={` h-[400px] w-[284px] dark:bg-pagina-dark cursor-pointer rounded-[10px] hover:scale-[0.99] dark:hover:text-pagina-dark  flex  flex-col justify-center gap-[60px] items-center`}
+        style={{ border: `2px solid ${color}`, color: hover? "": color, backgroundColor: hover? color : "", transition:"all 0.5s linear"}} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
       >
         <div
-          className="w-[90px] h-[90px] z-10 bg-white rounded-full  shadowMarquee flex items-center justify-center"
+          className="w-[90px] h-[90px] z-10 bg-white dark:bg-pagina-dark  rounded-full  shadowMarquee flex items-center justify-center"
           style={{ border: `2px solid ${color}` }}
         >
-          <Image src={src} height={height} width={width} alt={label} />
+          <Image src={src} height={height} width={width} alt={label}   />
         </div>
         <div>
           <h1 className="text-3xl font-roboto">{label}</h1>
