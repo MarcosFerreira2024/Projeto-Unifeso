f1654a8d0fbfc29a4f14ed718927181837af272a
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-20 20:25:24 -0300

    dark mode texto

--- src/components/geral/Texto.tsx
+++ src/components/geral/Texto.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Texto({texto,color}:{texto:string,color?:string}) {
   return (
     <div>
-      <p className="max-w-[540px] min-w-[200px] font-roboto font-normal mt-5 lg:mt-0 text-sm sm:text-base  text-subtitle" style={{color:color}}>{texto}</p>
+      <p className="max-w-[540px] min-w-[200px] font-roboto font-normal mt-5 dark:text-subtitle-dark lg:mt-0 text-sm sm:text-base  text-subtitle" style={{color:color}}>{texto}</p>
     </div>
   );
 }
