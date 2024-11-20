38cced9c1ddb393fbe7a7b1cdac6618fd8b9c4e5
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-20 20:22:45 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,29 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        background: "#006B64",
-        title: "#006B64",
-        subtitle: "#04887F", 
+        background: {
+          DEFAULT: "#006B64",
+          dark: "#374151",
+        },
+        title: {
+          DEFAULT: "#006B64",
+          dark: "#F3F4F6",
+        },
+        subtitle: {
+          DEFAULT: "#04887F",
+          dark: "#D1D5DB",
+        },
+        pagina: {
+          DEFAULT: "#FFFFFF",
+          dark: "#111827",
+        },
       },
       fontFamily: {
-        roboto: ['var(--font-family-main)', 'sans-serif'],
+        roboto: ["var(--font-family-main)", "sans-serif"],
       },
     },
   },
