3a489817beb0c7f18a1f403aec5032a8a35cae25
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2024-11-24 20:55:44 -0300

    Create AgendamentoPage.tsx

---
+++ src/app/agendamento/AgendamentoPage.tsx
@@ -0,0 +1,18 @@
+import NavBar from '@/components/navbar/NavBar'
+import React from 'react'
+import Select from './FormAgenda'
+import Footer from '@/components/footer/Footer'
+
+function AgendamentoPage() {
+  return (
+    <>
+     <NavBar visible={false}/>
+     <Select />
+     <Footer />   
+    
+    
+    </>
+  )
+}
+
+export default AgendamentoPage
